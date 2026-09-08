cd7780cc53c745ca29f012b156ac0258bff5da0d
jsquintero-co <sebasqgmusic@gmail.com>
2026-09-07 19:06:59 -0500

    Modify stadium capacity constraint to allow 40000
    Updated the capacity constraint to allow a minimum of 40000.

--- sql/entrega1/ddl/ddl_modelo_inicial.sql
+++ sql/entrega1/ddl/ddl_modelo_inicial.sql
@@ -33,7 +33,7 @@ CREATE TABLE ESTADIO (
         REFERENCES EDICION_MUNDIAL(id_edicion),
 
     CONSTRAINT ck_estadio_capacidad
-        CHECK (capacidad > 40000)
+        CHECK (capacidad >= 40000) --Mínimo que permite la fifa por estadio
 );
 
 -- TABLA: SELECCION
